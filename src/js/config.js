e9cc199cf960aa0e03e7e2319ed16cc3934848f4
Kazuki Murahama <kazuki.murahama@gmail.com>
2016-08-30 20:31:42 +0900

    support json data for config #23

--- src/js/config.js
+++ src/js/config.js
@@ -2,42 +2,29 @@ jQuery.noConflict();
 
 (($, PLUGIN_ID) => {
 
-  var arrColumnConfig = (config) => {
-    var result = [];
-    Object.keys(config).forEach((key) => {
-      if (key.substring(0, 6) === "column") {
-        result.push(config[key]);
-      }
-    });
-    return result;
-  }
-
   var config = kintone.plugin.app.getConfig(PLUGIN_ID);
   var fields = [];
 
   $(document).ready(() => {
-    kintone.api(kintone.api.url('/k/v1/preview/form', true), 'GET', {
+    kintone.api(kintone.api.url('/k/v1/preview/form', true), 'GET', { 
       'app': kintone.app.getId()
     }, (resp) => {
       fields = [];
       Object.keys(resp.properties).forEach((i, key) => {
         if ('code' in resp.properties[key]) {
-          fields.push(resp.properties[key].code);
+          fields.push(resp.properties[key]);
         }
       });
       console.dir(fields);
-      var options = fields.map((i, key) => {
-        return {text: fields[key], value: fields[key]};
-      });
 
-      var columns = arrColumnConfig(config);
-      // 未設定の場合はデフォルト設定
-      if (columns.length <= 0) columns = [options[0].value];
-       
+      var columns = config.columns ? JSON.parse(config.columns) : [];
+      // 未設定の場合はデフォルト設定 
+      if (columns.length <= 0) columns = [fields[0]];
+
       var vue = new Vue({
         el: '#form',
         data: {
-          options: options,
+          options: fields,
           columns: columns,
           elementId: 'elementId' in config ? config.elementId : '',
         },
@@ -46,20 +33,18 @@ jQuery.noConflict();
             this.columns.splice(index, 1);
           },
           addColumn(index) {
-            this.columns.splice(index, 0, this.options[0].value);
+            this.columns.splice(index, 0, this.options[0]);
           },
           registConfig() {
             var config = {};
-            this.columns.forEach((column, i) => {
-              config[`columns${i}`] = column;
-            });
+            config['columns'] = JSON.stringify(columns);
             config['elementId'] = this.elementId;
             kintone.plugin.app.setConfig(config);
           },
           cancel() {
             history.back();
           }
-        },
+        }
       });
     })
   });
